4ff4b916d4283cc4865b2b8f51121bd99beb4d93
Mana <57663038+4d616e61@users.noreply.github.com>
2025-02-05 17:45:29 -0800

    fix import

--- main.ts
+++ main.ts
@@ -5,22 +5,16 @@ import { SyncDB } from "./sync/db.ts";
 import { TaskType } from "./sync/task.ts";
 import { sleep } from "./utils/utils.ts";
 import { parse } from "@std/jsonc";
-import { api, scrape, sync } from "./config.json" with { type: "json" };
+import config from "./config.json" with { type: "json" };
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  // i hate this so much
-  const decoder = new TextDecoder("utf-8");
-
-  const _config_dat = Deno.readFileSync("./config.jsonc");
-  const config = parse(decoder.decode(_config_dat));
-  assert(config != null);
   const sdb: SyncDB = new SyncDB(
     "./syncdb.db",
-    sync.autogen_new_tasks,
-    sync.max_generated_tasks,
+    config.sync.autogen_new_tasks,
+    config.sync.max_generated_tasks,
     //placeholder
     0,
-    sync.max_ids_per_task,
+    config.sync.max_ids_per_task,
   );
   const ddb: DataDB = new DataDB("./datadb.db");
   const scraper_norm: Scraper = new Scraper(sdb, ddb, TaskType.NORM);
@@ -29,12 +23,12 @@ if (import.meta.main) {
   console.log(`GID: ${max_gid}`);
   sdb.max_id = max_gid;
   //const max_gid = 1000000;
-  if (scrape.use_auth) {
+  if (config.scrape.use_auth) {
     const scraper_authed: Scraper = new Scraper(
       sdb,
       ddb,
       TaskType.EXH,
-      scrape.cookies,
+      config.scrape.cookies,
     );
     scraper_authed.pagination_loop();
   }
